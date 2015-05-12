a8538153fe92724c7fdad03ea82761f7ad9b92d5
Employee-Project <datta.sumit03@gmail.com>
2015-05-12 17:06:16 +0600

    calculator

---
+++ SalaryCalculatorApi/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace SalaryCalculatorApi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.employeeName = new System.Windows.Forms.TextBox();
+            this.basicAmount = new System.Windows.Forms.TextBox();
+            this.houseRent = new System.Windows.Forms.TextBox();
+            this.medicalAllowance = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.showSalaryButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Employee Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Basic Amount:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(54, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "House Rent:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(57, 181);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Medical Allowance";
+            // 
+            // employeeName
+            // 
+            this.employeeName.Location = new System.Drawing.Point(181, 41);
+            this.employeeName.Name = "employeeName";
+            this.employeeName.Size = new System.Drawing.Size(215, 20);
+            this.employeeName.TabIndex = 4;
+            // 
+            // basicAmount
+            // 
+            this.basicAmount.Location = new System.Drawing.Point(181, 87);
+            this.basicAmount.Name = "basicAmount";
+            this.basicAmount.Size = new System.Drawing.Size(215, 20);
+            this.basicAmount.TabIndex = 5;
+            // 
+            // houseRent
+            // 
+            this.houseRent.Location = new System.Drawing.Point(181, 129);
+            this.houseRent.Name = "houseRent";
+            this.houseRent.Size = new System.Drawing.Size(215, 20);
+            this.houseRent.TabIndex = 6;
+            // 
+            // medicalAllowance
+            // 
+            this.medicalAllowance.Location = new System.Drawing.Point(181, 181);
+            this.medicalAllowance.Name = "medicalAllowance";
+            this.medicalAllowance.Size = new System.Drawing.Size(215, 20);
+            this.medicalAllowance.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(427, 135);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "% of Basic";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(430, 187);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "% of Basic";
+            // 
+            // showSalaryButton
+            // 
+            this.showSalaryButton.Location = new System.Drawing.Point(260, 249);
+            this.showSalaryButton.Name = "showSalaryButton";
+            this.showSalaryButton.Size = new System.Drawing.Size(125, 23);
+            this.showSalaryButton.TabIndex = 10;
+            this.showSalaryButton.Text = "Show Me Salary";
+            this.showSalaryButton.UseVisualStyleBackColor = true;
+            this.showSalaryButton.Click += new System.EventHandler(this.showSalaryButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(580, 291);
+            this.Controls.Add(this.showSalaryButton);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.medicalAllowance);
+            this.Controls.Add(this.houseRent);
+            this.Controls.Add(this.basicAmount);
+            this.Controls.Add(this.employeeName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Salary Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox employeeName;
+        private System.Windows.Forms.TextBox basicAmount;
+        private System.Windows.Forms.TextBox houseRent;
+        private System.Windows.Forms.TextBox medicalAllowance;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button showSalaryButton;
+    }
+}
+
